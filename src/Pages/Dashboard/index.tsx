61f2ad156b1e75d4accd5204e3cea580fa73cf24
Filippo Barcellos <barcellos.filippo@gmail.com>
2021-01-31 22:01:03 +0000

    home page

---
+++ src/Pages/Dashboard/index.tsx
@@ -0,0 +1,71 @@
+import React, { useState, FormEvent } from 'react';
+import { FiChevronRight } from 'react-icons/fi'
+import Loader from 'react-loader-spinner';
+import api from '../../services/api';
+
+import logo from '../../assets/logo.svg';
+
+import { Title, Form, UserList, Error } from './styles';
+
+interface User {
+  id: number,
+  avatar_url: string,
+  login: string,
+}
+
+const Dashboard: React.FC  = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState('');
+  const [user, setUser] = useState<User[]>([]);
+
+  async function handleAddRepository(event: FormEvent<HTMLFormElement>): Promise<void> {
+    event.preventDefault();
+    if (!searchTerm) {
+      setIsError('Please type a github username');
+      return;
+    };
+
+    try {
+      setIsLoading(true);
+
+      const response = await api.get(`search/users?q=${searchTerm}`);
+      setUser(response.data.items);
+      setSearchTerm('');
+      setIsError('');
+    } catch (err) {
+      setIsLoading(false);
+      setIsError('Something is wrong. Please try again!');
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  return (
+    <>
+      <img src={logo} alt="Github Explorer"/>
+      <Title>Explore github's repositories</Title>
+
+      <Form onSubmit={handleAddRepository} error={!!isError}>
+        <input value={searchTerm} onChange={e => setSearchTerm(e.target.value)} type="search" placeholder="Type repository name" />
+        <button type="submit">{isLoading ? <Loader type="ThreeDots" color="#fff" height={40} width={40}/> : 'Search'}</button>
+      </Form>
+
+      {isError && <Error>{isError}</Error>} 
+
+      <UserList>
+        {user.map(user => (
+          <a href="test" key={user.id}>
+            <img src={user.avatar_url} alt={user.login} />
+            <div>
+              <strong>{user.login}</strong>
+            </div>
+            <FiChevronRight size={20} />
+          </a>
+        ))}
+      </UserList>
+    </>
+  )
+}
+
+export default Dashboard;
---
+++ src/Pages/Dashboard/styles.ts
@@ -0,0 +1,108 @@
+import styled from 'styled-components';
+import { shade } from 'polished';
+
+interface FormProps {
+  error: boolean;
+}
+ 
+export const Title = styled.h1`
+  font-size: 48px;
+  color: #3a3a3a;
+  margin-top: 80px;
+  max-width: 450px;
+  line-height: 56px;
+`
+
+export const Form = styled.form<FormProps>`
+  margin-top: 40px;
+  max-width: 700px;
+  display: flex;
+
+  input {
+    flex: 1;
+    height: 50px;
+    padding: 0 24px;
+    border-radius: 5px 0 0 5px;
+    border: 1px solid ${props => props.error ? '#c53030' : ' #fff'};
+    color: #3a3a3a;
+
+    &::placeholder {
+      color: #a8a8b3
+    }
+  }
+
+  button {
+    width: 200px;
+    height: 50px;
+    border: 0;
+    background: #04d361;
+    border-radius: 0px 5px 5px 0px;
+    cursor: pointer;
+    color: #fff;
+    font-weight: bold;
+    transition: background-color 0.2s;
+
+    &:hover {
+      background: ${shade(0.2, "#04d361")};
+    }
+  }
+`;
+
+export const UserList = styled.div`
+  margin-top: 80px;
+  max-width: 700px;
+  
+  a {
+    background: #fff;
+    border-radius: 5px;
+    width: 100%;
+    padding: 24px;
+    text-decoration: none;
+    display: flex;
+    align-items: center;
+    transition: transform 0.2s;
+
+    & + a {
+      margin-top: 16px;
+    }
+
+    &:hover {
+      transform: translateX(10px);
+    }
+  }
+
+  img {
+    width: 64px;
+    height: 64px;
+    border-radius: 50%;
+  }
+
+  div {
+    margin-left: 16px;
+    display: flex;
+    flex-direction: column;
+
+    strong {
+      font-size: 18px;
+      color: #3d3d4d;
+    }
+
+    span {
+      font-size: 15px;
+      color: #a8a8b2;
+      margin-top: 4px;
+    }
+  }
+
+  svg {
+    margin-left: auto;
+    color: #cbcbd6;
+  }
+`;
+
+export const Error = styled.span`
+  display: block;
+  color: #c53030;
+  margin-top: 16px;
+  font-size: 15px;
+`
